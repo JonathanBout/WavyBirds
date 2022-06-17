a8ba8e4baa77a624d69345e40ab366d0613464fa
DutchAerospace <jonathanbout01@gmail.com>
2022-06-17 21:38:45 +0200

    Flappy Bird

--- WavyBirds/WavyBirds.py
+++ WavyBirds/WavyBirds.py
@@ -1,33 +1,91 @@
-import sys, pygame
-pygame.init()
+import random
+import pygame
+from datetime import datetime, timedelta
 
-size = width, height = 600, 400
-speed = 0.01
-white = 255, 255, 255
-location = 0
-screen = pygame.display.set_mode(size)
+def gameover():
+    print("Game over!")
+    pygame.time.wait(1000)
+    menu()
 
-ball = pygame.image.load("vogel.png")
-ballrect = ball.get_rect()
-pipe = pygame.image.load("pijp.png")
-piperect = pipe.get_rect()
-piperect.bottom = height
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-    print(speed)
-    location += speed
-    piperect = piperect.move(location - piperect.left, 0)
-    if location > width:
-        location = 0
-    screen.fill(white)
-    screen.blit(pipe, piperect)
-    speed += .0001
-    #ballrect = ballrect.move(speed)
-    #if ballrect.left < 0 or ballrect.right > width:
-    #    speed[0] = -speed[0]
-    #if ballrect.top < 0 or ballrect.bottom > height:
-    #    speed[1] = -speed[1]
-    #screen.fill(white)
-    #screen.blit(ball, ballrect)    
-    pygame.display.flip()
+def game() :
+    background = 75, 75, 250
+    size = width, height = 700, 500
+    screen = pygame.display.set_mode(size)
+    gravity = .4
+    pipegapsize = 125
+
+    birdrect = pygame.Rect(width/2-20, 0, 40, 40)
+    birdrect.top = 0
+    pygame.init()
+
+    speed = 0.05
+    location = 0
+    birdlocation = 0
+    birdspeed = 0
+    toppipe = pygame.Rect(0, 0, 20, 0)
+    bottompipe = pygame.Rect(0, 0, 20, random.randint(10, height - pipegapsize + 10))
+
+    bottompipe.bottom = height
+    toppipe.h = 1600
+    toppipe.bottom = bottompipe.top - pipegapsize
+
+    toppipe.left = width
+    bottompipe.left = width
+    prevtime = datetime.now()
+    run = True
+    deltatime = timedelta(0)
+    while run:
+        didbirdmove = False
+        #beweging van de pijpen en de vogel
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    didbirdmove = True
+                    birdspeed = -.2
+        if didbirdmove == False:
+            birdspeed += gravity * deltatime.total_seconds()
+            
+        location += speed * deltatime.total_seconds()
+        birdlocation += birdspeed
+
+        toppipe = toppipe.move((width - location) - toppipe.left, 0)
+        bottompipe = bottompipe.move((width - location) - bottompipe.left, 0)
+        birdrect = birdrect.move(0, birdlocation - birdrect.top)
+
+        if location > width + toppipe.w:
+            location = 0
+
+            bottompipe.h = random.randint(10, height - pipegapsize + 10)
+            bottompipe.bottom = height
+            toppipe.bottom = bottompipe.top - pipegapsize
+
+
+
+        #renderen
+        screen.fill(background)
+        pygame.draw.rect(screen, (0, 100, 50), birdrect)
+        pygame.draw.rect(screen, (100, 100, 50), toppipe)
+        pygame.draw.rect(screen, (255, 100, 50), bottompipe)
+        speed += deltatime.microseconds/100000
+        pygame.display.flip()
+        deltatime = datetime.now() - prevtime
+        prevtime = datetime.now()
+
+        #botsingen checken
+        if birdrect.bottom > height:
+            gameover()
+            run = False
+
+        if birdrect.colliderect(toppipe):
+            run = False
+            gameover()
+        if birdrect.colliderect(bottompipe):
+            run = False
+            gameover()
+
+def menu():
+    game()
+
+menu()
+    
